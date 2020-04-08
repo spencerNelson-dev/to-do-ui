3aa45a6770a4e9df21309e486952a40c559c6d62
Spencer Nelson <spencernelson144@gmail.com>
2020-04-08 13:58:06 -0600

    organized fetchUtils.js

--- src/fetchUtils.js
+++ src/fetchUtils.js
@@ -5,6 +5,9 @@ import jwt from 'jsonwebtoken'
 This file contains all the fetch requests used in our react app
 */
 
+/* fetch requests regarding tasks */
+//#region 
+
 // GET all the tasks by user id
 export const getTasksByUserId = (user) => {
 
@@ -27,28 +30,7 @@ export const getTasksByUserId = (user) => {
         })
 }
 
-// GET all users
-export const getAllUsers = (token) => {
-
-    return fetch(`${uriBase}${userApi}`, {
-        method: 'GET',
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        }
-    })
-    .then(httpResult => {
-        if(!httpResult.ok){
-            throw new Error("Bad response")
-        }
-
-        return httpResult.json()
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
+// get all tasks regarless of users
 export const getAllTasks = (token) => {
 
     return fetch(`${uriBase}${currentApi}`, {
@@ -74,78 +56,6 @@ export const getAllTasks = (token) => {
     })
 }
 
-// POST - create new user
-export const createNewUser = (user, token) => {
-
-    let createObj = {}
-    createObj.doc = user
-
-    return fetch(`${uriBase}${userApi}/create`,{ 
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(createObj)
-    })
-    .then(httpResult => {
-        if(!httpResult.ok){
-            throw new Error("CreateNewUser Failed")
-        }
-
-        return httpResult.json()
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-export const createNewUserNoToken = async (user) => {
-
-    //make a token
-    let token = await jwt.sign({email:"spencernelson144@gmail.com"}, JWT_KEY)
-
-    let createObj = {}
-    createObj.doc = user
-
-    return fetch(`${uriBase}${userApi}/create`,{ 
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(createObj)
-    })
-    .then(httpResult => {
-        if(!httpResult.ok){
-            throw new Error("CreateNewUser Failed")
-        }
-
-        return httpResult.json()
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-// delete user
-export const deleteUser = (user, token) => {
-
-    return fetch(`${uriBase}${userApi}/delete/${user._id}`,{
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        }
-    })
-    .then(httpResult => {
-        if (!httpResult.ok) {
-            throw new Error("Bad response")
-        }
-
-        return httpResult.json()
-    })
-}
-
 // POST - Create new task given a task object
 export const createNewTask = (newTask) => {
 
@@ -166,30 +76,6 @@ export const createNewTask = (newTask) => {
         .catch(error => {
             console.log(error)
         })
-}
-
-// PATCH - update user
-export const updateUser = (userId,updatedUser, token) => {
-
-    return fetch(`${uriBase}${userApi}/update/${userId}`, {
-        method: "PATCH",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(updatedUser)
-
-    })
-    .then(httpResult => {
-        if(!httpResult.ok){
-            throw new Error("Could not update user")
-        }
-
-        return httpResult.json()
-    })
-    .catch(error => {
-        console.log(error)
-    })
 }
 
 // PATCH - update task given task id and what to change
@@ -243,4 +129,131 @@ export const deleteTask = (taskId) => {
         return httpResult.json()
     })
 }
+
+//#endregion
+
+/* fetch requrests regarding users */
+//#region 
+
+// GET all users
+export const getAllUsers = (token) => {
+
+    return fetch(`${uriBase}${userApi}`, {
+        method: 'GET',
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+    })
+    .then(httpResult => {
+        if(!httpResult.ok){
+            throw new Error("Bad response")
+        }
+
+        return httpResult.json()
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+// POST - create new user
+export const createNewUser = (user, token) => {
+
+    let createObj = {}
+    createObj.doc = user
+
+    return fetch(`${uriBase}${userApi}`,{ 
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(createObj)
+    })
+    .then(httpResult => {
+        if(!httpResult.ok){
+            throw new Error("CreateNewUser Failed")
+        }
+
+        return httpResult.json()
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+// POST - create new user not signed in
+export const createNewUserNoToken = async (user) => {
+
+    //make a token
+    let token = await jwt.sign({email:"spencernelson144@gmail.com"}, JWT_KEY)
+
+    let createObj = {}
+    createObj.doc = user
+
+    return fetch(`${uriBase}${userApi}`,{ 
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(createObj)
+    })
+    .then(httpResult => {
+        if(!httpResult.ok){
+            throw new Error("CreateNewUser Failed")
+        }
+
+        return httpResult.json()
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+// delete user
+export const deleteUser = (user, token) => {
+
+    return fetch(`${uriBase}${userApi}/${user._id}`,{
+        method: "DELETE",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+    })
+    .then(httpResult => {
+        if (!httpResult.ok) {
+            throw new Error("Bad response")
+        }
+
+        return httpResult.json()
+    })
+}
+
+// PATCH - update user
+export const updateUser = (userId,updatedUser, token) => {
+
+    return fetch(`${uriBase}${userApi}/${userId}`, {
+        method: "PATCH",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedUser)
+
+    })
+    .then(httpResult => {
+        if(!httpResult.ok){
+            throw new Error("Could not update user")
+        }
+
+        return httpResult.json()
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+//#endregion
 
